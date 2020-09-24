6515f720854dc8e4c85033be97d9822636fecc30
A-K796 <69107060+A-K796@users.noreply.github.com>
2020-09-24 19:34:55 +0900

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,11 @@ class BooksController < ApplicationController
 	def create
 		@book =Book.new(book_params)
 		if @book.save
-			redirect_to books_path
+			flash[:notice] = "Book was successfully created."
+			redirect_to book_path(@book)
 		else
+			# renderを使うときはインスタンス変数も設定する。
+			@books = Book.all
 			render 'index'
 		end
 	end
@@ -24,8 +27,11 @@ class BooksController < ApplicationController
 	def update
 		@book = Book.find(params[:id])
 		if @book.update(book_params)
+			flash[:notice] = "Book was successfully updated."
 			redirect_to book_path(@book)
 		else
+			# renderを使うときはインスタンス変数も設定する。
+			@books = Book.all
 			render 'index'
 		end
 	end
